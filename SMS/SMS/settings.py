eb7208247c2705296e9e2b1f9ad2184c4b4c600f
jinalladani25 <50820489+jinalladani25@users.noreply.github.com>
2021-12-28 18:10:37 -0800

    management

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'SMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MySocity_Management_System',
+        'USER': 'jinal',
+        'PASSWORD': 'jinal123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -132,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
