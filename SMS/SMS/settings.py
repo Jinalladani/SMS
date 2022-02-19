4f86fe9b9476d12004d70633b45cbfae96f71388
Yash Kathrotiya <yashkathrotiya7048@gmail.com>
2022-02-19 22:16:28 +0530

    minor production changes

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -90,17 +90,24 @@ WSGI_APPLICATION = 'SMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'test',
+#         'USER': 'jinal',
+#         'PASSWORD': 'jinal123',
+#         'HOST': '64.227.186.69',
+#         'PORT': '3306',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'jinal',
-        'PASSWORD': 'jinal123',
-        'HOST': '64.227.186.69',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
